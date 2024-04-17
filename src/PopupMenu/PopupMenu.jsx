b3b76a3a1b3ff70b4b32fd21c707137d910a9b38
Sankalp Ramteke <sankalprt658@gmail.com>
2024-04-17 17:38:09 +0530

    popUp Icons Added

--- src/PopupMenu/PopupMenu.jsx
+++ src/PopupMenu/PopupMenu.jsx
@@ -1,12 +1,20 @@
 import React, { useState } from 'react';
-import { IoClose, IoMenu } from 'react-icons/io5'; // Import close and menu icons from react-icons
-import styles from './Popupmenu.module.css'; // Assuming styles are in PopupMenu.module.css
+import { IoClose, IoMenu } from 'react-icons/io5'; 
+import styles from './Popupmenu.module.css'; 
 import { HiOutlineDesktopComputer } from "react-icons/hi";
 import { TbArrowsDoubleNeSw } from "react-icons/tb";
 import { MdSmartButton } from "react-icons/md";
 import { MdOutlineKeyboardArrowRight } from "react-icons/md";
 import { RxCross1 } from "react-icons/rx";
 import { BsBoxArrowUpRight } from "react-icons/bs";
+import { CiBank } from "react-icons/ci";
+import { IoNewspaperOutline } from "react-icons/io5";
+import { TiDownloadOutline } from "react-icons/ti";
+import { CiPercent } from "react-icons/ci";
+import { MdOutlineMenuBook } from "react-icons/md";
+import { IoPeopleOutline } from "react-icons/io5";
+import { BiSupport } from "react-icons/bi";
+
 
 const PopupMenu = () => {
     const [isOpen, setIsOpen] = useState(false);
@@ -30,21 +38,24 @@ const PopupMenu = () => {
                            
                         </ul>
                         <p>Explore Payment Suite<MdOutlineKeyboardArrowRight /></p>
+                         <div className={styles.container}>
                         <h3>Not sure which product to choose?</h3>
+                       
                         <button className={styles.btn}>Find me a product<BsBoxArrowUpRight style={{marginLeft:"10px"}}/></button>
-                        <h2>RAZORPAY X-BANKING SUITE</h2>
+                        </div>
+                        <h2 className={styles.suite}>RAZORPAY X-BANKING SUITE</h2>
                         <ul>
-                            <li>Current Accounts</li>
-                            <li>Vendor Payments</li>
-                            <li>Payroll</li>
+                            <li><CiBank/>Current Accounts</li>
+                            <li><IoNewspaperOutline/> Vendor Payments</li>
+                            <li><TiDownloadOutline/>Payroll</li>
                             
                         </ul>
                         <h5>Explore Banking Suite</h5>
                         <ul>
-                            <li>Pricing</li>
-                            <li>Resources</li>
-                            <li>Partners</li>
-                            <li>Support</li>
+                            <li><CiPercent/>Pricing</li>
+                            <li><MdOutlineMenuBook/>Resources</li>
+                            <li><IoPeopleOutline/>Partners</li>
+                            <li><BiSupport/>Support</li>
                         </ul>
                         <button className={styles.loginButton}>Login</button>
                     </div>
